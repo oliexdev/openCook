00a3e69c99c2b1ffed583d4d352ac6d8d5790a63
oliexdev <olie.xdev@googlemail.com>
2026-08-09 15:30:25 +0200

    update openCook version to 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.food.opencook"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
